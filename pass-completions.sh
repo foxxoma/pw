6a4265c34e44076f219509bbbce4715f1715e877
Andrey <foxxoma@gmail.com>
2023-06-21 14:17:42 +0300

    method (-) pass tab

--- pass-completions.sh
+++ pass-completions.sh
@@ -1,12 +1,10 @@
 #/usr/bin/env bash
 _pass_completions()
 {
-  if [ "${#COMP_WORDS[@]}" -lt 2 ] ; then
-    COMPREPLY=($(compgen -W "list get add del ssh help" "${COMP_WORDS[1]}"))
-  elif [[ "${COMP_WORDS[1]}" == "get" || "${COMP_WORDS[1]}" == "del" ]] ; then
-    COMPREPLY=($(compgen -W "$(cat $PW_PATH/pw.txt | awk -F ':::::' 'BEGIN { ORS=" " }; {print $1}')" -- "${COMP_WORDS[2]}"))
+  if [[ "${COMP_WORDS[1]}" == "-" ]] ; then
+    COMPREPLY=($(compgen -W "-list -add -rm -ssh -help" "${COMP_WORDS[1]}"))
   else
-    COMPREPLY=()
+    COMPREPLY=($(compgen -W "$(cat $PW_PATH/pw.txt | awk -F ':::::' 'BEGIN { ORS=" " }; {print $1}')" -- "${COMP_WORDS[2]}"))
   fi
 }
 
